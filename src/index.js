840f84a5fe0815714d30deb73ec5e060b81369ea
Aditya <aditya-tyagi@outlook.com>
2019-03-29 00:12:50 -0500

    yo

--- src/index.js
+++ src/index.js
@@ -6,6 +6,22 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
+/**
+ * setting up express middleware
+ */
+// app.use((req, res, next)=>{
+//     if(req.method == 'GET'){
+//         console.log('GET is unabled')
+//     }else{
+//         next()
+//     }
+// })
+
+// app.use((req, res, next)=>{
+//     res.status(503).send('Under Maintainece')
+// })
+
+
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
@@ -13,6 +29,3 @@ app.use(taskRouter)
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
-
-const jwt = require('jsonwebtoken')
-
--- src/routers/user.js
+++ src/routers/user.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const User = require('../models/user')
 const router = new express.Router()
+const auth = require('../middleware/auth')
 
 // console.log(req.body);
 router.post('/users', async (req, res) => {
@@ -8,7 +9,8 @@ router.post('/users', async (req, res) => {
 
     try {
         await user.save()
-        res.status(201).send(user)
+        let token = await user.generateAuthToken()
+        res.status(201).send({user,token})
     } catch (e) {
         res.status(400).send(e)
     }
@@ -24,13 +26,8 @@ router.post('/users/login', async(req, res)=>{
     }
 } )
 
-router.get('/users', async (req, res) => {
-    try {
-        const users = await User.find({})
-        res.send(users)
-    } catch (e) {
-        res.status(500).send()
-    }
+router.get('/users/me', auth,async (req, res) => {
+    res.send(req.user)
 })
 
 router.get('/users/:id', async (req, res) => {
